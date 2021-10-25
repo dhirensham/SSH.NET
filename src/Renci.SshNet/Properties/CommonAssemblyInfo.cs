96ffb65c6527097491d77a3cb9c5bb006de0b9eb
Maurizio Macagno <mac@sensoriahealth.com>
2021-10-25 12:14:07 -0700

    Branch out for publishing a Nuget Package from forked repo

--- src/Renci.SshNet/Properties/CommonAssemblyInfo.cs
+++ src/Renci.SshNet/Properties/CommonAssemblyInfo.cs
@@ -2,16 +2,16 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("SSH.NET is a Secure Shell (SSH) library for .NET, optimized for parallelism.")]
+[assembly: AssemblyDescription("SSH.NET is a Secure Shell (SSH) library for .NET, optimized for parallelism.\r\nThis is a fork from original repository with explicit .NET Core 3.1 and .NET 5.0 flavors, to overcome a dotnet core runtime issue when using the .NET standard library.")]
 [assembly: AssemblyCompany("Renci")]
 [assembly: AssemblyProduct("SSH.NET")]
 [assembly: AssemblyCopyright("Copyright © Renci 2010-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2020.0.1")]
-[assembly: AssemblyFileVersion("2020.0.1")]
-[assembly: AssemblyInformationalVersion("2020.0.1")]
+[assembly: AssemblyVersion("2021.10.1")]
+[assembly: AssemblyFileVersion("2021.10.1")]
+[assembly: AssemblyInformationalVersion("2021.10.1")]
 [assembly: CLSCompliant(false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
